74b680040e4a6ae878061d12bf6696dc80e4950a
AviiNL <me@avii.nl>
2022-11-15 22:19:39 +0100

    Arithmatic operators on character and number (+ and -)

--- alang-lib/src/types/character.rs
+++ alang-lib/src/types/character.rs
@@ -30,26 +30,74 @@ impl BinaryOperation for CharacterVal {
         operator: Operator,
     ) -> Result<RuntimeValue, BinaryOperationError> {
         let other = match &other_raw {
-            RuntimeValue::Character(s) => s,
-            _ => return Err(BinaryOperationError::InvalidOperationType),
+            RuntimeValue::Character(s) => Some(s),
+            _ => None,
         };
 
-        match operator {
-            Operator::Arithmatic(Arithmatic::Plus) => {
-                let mut new_value = String::from(self.value);
-                new_value.push_str(&other.value.to_string());
-                Ok(RuntimeValue::String(StringVal { value: new_value }))
-            }
-            // Equal
-            Operator::Relational(Relational::Equal) => Ok(RuntimeValue::Boolean(BooleanVal {
-                value: self.value == other.value,
-            })),
-            // Not Equal
-            Operator::Relational(Relational::NotEqual) => Ok(RuntimeValue::Boolean(BooleanVal {
-                value: self.value != other.value,
-            })),
-            _ => Err(BinaryOperationError::InvalidOperator),
+        if other.is_some() {
+            let other = other.unwrap();
+            return match operator {
+                Operator::Arithmatic(Arithmatic::Plus) => {
+                    let mut new_value = String::from(self.value);
+                    new_value.push_str(&other.value.to_string());
+                    Ok(RuntimeValue::String(StringVal { value: new_value }))
+                }
+                // Equal
+                Operator::Relational(Relational::Equal) => Ok(RuntimeValue::Boolean(BooleanVal {
+                    value: self.value == other.value,
+                })),
+                // Not Equal
+                Operator::Relational(Relational::NotEqual) => {
+                    Ok(RuntimeValue::Boolean(BooleanVal {
+                        value: self.value != other.value,
+                    }))
+                }
+                _ => Err(BinaryOperationError::InvalidOperator),
+            };
         }
+
+        // math on characters with numbers
+        let other = match &other_raw {
+            RuntimeValue::Number(s) => Some(s),
+            _ => None,
+        };
+
+        if other.is_some() {
+            let other = other.unwrap();
+            return match operator {
+                Operator::Arithmatic(Arithmatic::Plus) => {
+                    // get numeric value of character
+                    let mut new_value = self.value as u8;
+                    new_value += other.value as u8;
+
+                    // back to character
+                    let new_value = new_value as char;
+                    Ok(RuntimeValue::Character(CharacterVal { value: new_value }))
+                }
+                Operator::Arithmatic(Arithmatic::Minus) => {
+                    // get numeric value of character
+                    let mut new_value = self.value as u8;
+                    new_value -= other.value as u8;
+
+                    // back to character
+                    let new_value = new_value as char;
+                    Ok(RuntimeValue::Character(CharacterVal { value: new_value }))
+                }
+                // Equal
+                Operator::Relational(Relational::Equal) => Ok(RuntimeValue::Boolean(BooleanVal {
+                    value: self.value as i32 == other.value as i32,
+                })),
+                // Not Equal
+                Operator::Relational(Relational::NotEqual) => {
+                    Ok(RuntimeValue::Boolean(BooleanVal {
+                        value: self.value as i32 != other.value as i32,
+                    }))
+                }
+                _ => Err(BinaryOperationError::InvalidOperator),
+            };
+        }
+
+        Err(BinaryOperationError::InvalidOperationType)
     }
 }
 
